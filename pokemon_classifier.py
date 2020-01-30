09731814e5af8a2d6c60d1fdd9df1eb991f5f726
ralterman <ralt@umich.edu>
2020-01-30 18:11:26 -0500

    choose model

--- pokemon_classifier.py
+++ pokemon_classifier.py
@@ -3,6 +3,14 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.neighbors import KNeighborsClassifier
+import imblearn
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import svm, tree
+import xgboost
 
 pd.options.display.max_rows = 1000
 
@@ -139,4 +147,89 @@ ax = sns.heatmap(pokemon.corr(), annot=True, cmap='Greens')
 bottom, top = ax.get_ylim()
 ax.set_ylim(bottom + 0.5, top - 0.5)
 
-pd.plotting.scatter_matrix(pokemon.iloc[:, 2:], figsize=(25,8))
+pd.plotting.scatter_matrix(pokemon.iloc[:, 2:], figsize=(25,10))
+
+
+def multi_hist_plot(df_1, df_2, to_plot):
+    fig = plt.figure(figsize=(10,5))
+    plt.style.use('seaborn')
+    sns.set_palette('bright')
+    hist_1 = sns.distplot(df_1[to_plot], kde = False)
+    hist_2 = sns.distplot(df_2[to_plot], kde = False)
+    plt.show()
+
+df_1 = pokemon[pokemon['Legendary'] == 1]
+df_2 = pokemon[pokemon['Legendary'] == 0]
+
+
+hp_hist = multi_hist_plot(df_1, df_2, 'HP')
+attack_hist = multi_hist_plot(df_1, df_2, 'Attack')
+defense_hist = multi_hist_plot(df_1, df_2, 'Defense')
+spattack_hist = multi_hist_plot(df_1, df_2, 'Special_Attack')
+spdefense_hist = multi_hist_plot(df_1, df_2, 'Special_Defense')
+speed_hist = multi_hist_plot(df_1, df_2, 'Speed')
+height_hist = multi_hist_plot(df_1, df_2, 'Height_M')
+weight_hist = multi_hist_plot(df_1, df_2, 'Weight_KG')
+
+#-----------------------------------------------------------------------------------------------
+
+# Set variables in order for train test split to be done
+y = pokemon['Legendary']
+x = pokemon[['HP', 'Attack', 'Defense', 'Special_Attack', 'Special_Defense', 'Speed', 'Height_M', 'Weight_KG']]
+X = pd.DataFrame(x, columns = x.columns)
+
+
+# Calculate train and test variables
+X_train , X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12)
+
+smote = SMOTE(sampling_strategy='minority')
+X_synth, y_synth = smote.fit_sample(X_train, y_train)
+
+
+classifiers = []
+
+model1 = xgboost.XGBClassifier()
+classifiers.append(model1)
+
+model2 = svm.SVC()
+classifiers.append(model2)
+
+model3 = tree.DecisionTreeClassifier()
+classifiers.append(model3)
+
+model4 = RandomForestClassifier()
+classifiers.append(model4)
+
+for clf in classifiers:
+    clf.fit(X_synth, y_synth)
+    y_pred= clf.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    print("Accuracy of %s is %s"%(clf, acc))
+    print("Precision of %s is %s"%(clf, prec))
+    print("Recall of %s is %s"%(clf, recall))
+    print("F1 Score of %s is %s"%(clf, f1))
+    print("")
+    cm = confusion_matrix(y_test, y_pred)
+    print(cm)
+    print("")
+    print(classification_report(y_test, y_pred))
+    print("")
+    print("")
+
+
+param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000],  'kernel': ['rbf', 'linear', 'poly', 'sigmoid']}
+grid = GridSearchCV(svm.SVC(), param_grid)
+grid_search = grid.fit(X_synth, y_synth)
+
+print (grid_search.best_params_)
+print (grid_search.best_estimator_)
+
+grid_predictions = grid_search.predict(X_test)
+grid.scorer_
+print(confusion_matrix(y_test, grid_predictions))
+print(classification_report(y_test, grid_predictions))
+
+model = svm.SVC(C=1000, kernel='rbf')
