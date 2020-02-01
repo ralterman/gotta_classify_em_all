81d4ad1b0544a6672fc010b02c54eebb8b4944da
ralterman <ralt@umich.edu>
2020-02-01 18:36:33 -0500

    commented code

--- pokemon.py
+++ pokemon.py
@@ -18,16 +18,19 @@ pd.options.display.max_rows = 1000
 pokemon = pd.read_csv('pokemon.csv', index_col = 0)
 pokemon
 
+
+# Load model
 model = pickle.load(open('model.pkl', 'rb'))
 
+# Set variables for train test split
 y = pokemon['Legendary']
 x = pokemon[['Pokemon_Name', 'HP', 'Attack', 'Defense', 'Special_Attack', 'Special_Defense', 'Speed', 'Height_M', 'Weight_KG']]
 X = pd.DataFrame(x, columns = x.columns)
 
-
-# Calculate train and test variables
 X_train , X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12)
 
+
+# Fit each model and find accuracy, precision, recall, and F1 score and print out confusion matrix and classification report
 y_pred= model.predict(X_test.drop('Pokemon_Name', axis=1))
 acc = accuracy_score(y_test, y_pred)
 prec = precision_score(y_test, y_pred)
@@ -44,21 +47,23 @@ print("")
 print(classification_report(y_test, y_pred))
 print("")
 print("")
-X_test
-y_pred
-y_test
 
+
+
+# Make dataframe from testing data
 pokemon2 = pd.DataFrame(y_test)
-pokemon2
 
+# Add prediction data to the dataframe for comparison (actual vs predicted)
 pokemon2 = pokemon2.rename(columns={"Legendary": "y_test"})
 pokemon2['y_pred'] = y_pred
-pokemon2
 
 
+# Concatenate that dataframe onto the main dataframe to match with pokémon names
 y = pd.concat([X_test, pokemon2], axis=1)
 y
 
+
+# Check if misclassified and return the names of the pokémon that are misclassified to a list but also 'None' if pokémon was not misclassified
 def misclassified(row):
     if row['y_test'] != row['y_pred']:
         return row['Pokemon_Name']
@@ -66,8 +71,7 @@ def misclassified(row):
 misclassifications = []
 misclassifications = y.apply(lambda x: misclassified(x), axis=1)
 
-misclassifications
-
+# Get ride of 'None' from list so only those that were misclassified remain
 missed = []
 for poke in misclassifications:
     if poke != None:
@@ -75,19 +79,22 @@ for poke in misclassifications:
 
 missed
 
+
+# Make new dataframe for just the misclassified pokémon with all of their attributes
 missed_list = pd.DataFrame()
 for item in missed:
     x = pd.DataFrame(pokemon.loc[pokemon['Pokemon_Name'] == item])
     missed_list = pd.concat([missed_list, x], axis=0)
 
+# Manually label whether the pokémon's misclassification was a False Positive or a False Negative
 missed_list['Label'] = ['False Neg', 'False Neg', 'False Pos', 'False Neg', 'False Pos', 'False Neg']
 
 missed_list
 
-a = missed_list.groupby(['label']).mean()
-a
 
+# Drop misclassified pokémon from original dataframe
 b = pokemon.drop([720, 763, 778, 793, 796, 808])
 
+# Get averages for each of the categories in the main dataframe, differentiating between legendary and non-legendary
 c = b.groupby(['Legendary']).mean()
 c
--- pokemon_classifier.py
+++ pokemon_classifier.py
@@ -18,22 +18,29 @@ import pickle
 pd.options.display.max_rows = 1000
 
 
+# Getting and cleaning data:
+
+# Scrape all pokémon and their attributes from pokemon.db
 url = 'https://pokemondb.net/pokedex/all'
 page = requests.get(url)
 data = pd.read_html(page.content)
 pokedex = pd.DataFrame(data[0])
-pokedex = pokedex.set_index('#')
-pokedex = pokedex.loc[~pokedex.index.duplicated(keep='first')]
+pokedex = pokedex.set_index('#') # Change index to pokédex number
+pokedex = pokedex.loc[~pokedex.index.duplicated(keep='first')] # Drop the different sprites of pokémon
 
+# Separate types to get Type1 and Type2 of each pokémon
 split_types = pokedex['Type'].str.split(' ', n = 1, expand = True)
 pokedex['Type1'] = split_types[0]
 pokedex['Type2'] = split_types[1]
 
+# Drop unnecessary columns, rename and reorder the rest
 pokedex = pokedex.drop(columns = ['Type', 'Total'])
 pokedex = pokedex.rename(columns={"Name": "Pokemon_Name", "Sp. Atk": "Special_Attack", "Sp. Def": "Special_Defense"})
 pokedex = pokedex.reindex(columns=['Pokemon_Name','Type1', 'Type2', 'HP', 'Attack', 'Defense', 'Special_Attack', 'Special_Defense', 'Speed'])
 
 
+# If specific sprite label remains after dropping duplicates, get rid of the extra parts of the pokémon name
+# Fix special pokémon name cases to allow for API calls to work
 def default_name(row):
     if len(row['Pokemon_Name'].split()) > 2:
         return row['Pokemon_Name'].split()[0]
@@ -51,6 +58,7 @@ def default_name(row):
 pokedex['Pokemon_Name'] = pokedex.apply(default_name, axis = 1)
 
 
+# Get url for each pokémon for PokéAPI calls from the PokéAPI
 def get_urls(num_of_pokemon):
     url_list = []
     url = 'https://pokeapi.co/api/v2/pokemon/?limit=' + str(num_of_pokemon)
@@ -59,9 +67,10 @@ def get_urls(num_of_pokemon):
     poke_list = poke_dict['results']
     return poke_list
 
+# Get each pokémon's height and weight from PokéAPI
 def get_height_weight(poke_url_list):
     info_list = []
-    info_list.append(('', ''))
+    info_list.append(('', '')) # Add empty row to beginning of list so indices match
     for item in poke_url_list:
         response2 = requests.get(item['url'])
         info_dict = response2.json()
@@ -73,16 +82,18 @@ def get_height_weight(poke_url_list):
 # hw_list = get_height_weight(get_urls(807))
 
 
+# Create new dataframe of just heights and weights and concatenate that to the end of the pokédex dataframe
 hw = pd.DataFrame(hw_list)
 hw = hw.rename(columns={0: 'Height_M', 1: 'Weight_KG'})
 pokemon = pd.concat([pokedex, hw], axis=1)
-pokemon = pokemon.drop(0, axis=0)
-pokemon = pokemon.drop(pokemon.index[807:])
+pokemon = pokemon.drop(0, axis=0) # Drop the first empty row
+pokemon = pokemon.drop(pokemon.index[807:]) # Drop all pokémon that were not on the PokéAPI
 
 
+# Get heights and weights for the few pokémon not on the PokéAPI but that you want to look at from serebii
 def get_height(row):
     pokemon = row['Pokemon_Name']
-    if len(pokemon.split()) == 2:
+    if len(pokemon.split()) == 2: # If pokémon name is two words, make it one for url - always make it all lowercase
         pokemon = row['Pokemon_Name'].replace(' ', '')
     base_url = 'https://www.serebii.net/pokemon/' + pokemon.lower() +'/'
     page = requests.get(base_url)
@@ -106,6 +117,7 @@ def get_weight(row):
     else:
         return item[-2].text.split('\t')[-1][:-2]
 
+# Make new dataframe for just those pokémon and concatenate that to the big dataframe
 pokemon2 = pokedex[807:810]
 pokemon2['Height_M'] = pokemon2.apply(lambda x: get_height(x), axis=1)
 pokemon2['Weight_KG'] = pokemon2.apply(lambda x: get_weight(x), axis=1)
@@ -116,6 +128,7 @@ pokemon = pokemon.shift()[1:]
 pokemon = pokemon.rename_axis('Pokedex_Num', axis='columns')
 
 
+# Find which pokémon are considered 'legendary' from serebii (each pokémon was the third item in the returned list from scraping)
 def get_legendary(url):
     idx = 0
     legend_list = []
@@ -130,6 +143,7 @@ def get_legendary(url):
 legends = get_legendary('https://www.serebii.net/pokemon/legendary.shtml')
 
 
+# Mark legendary pokémon with a 1 and non-legendary with a 0
 def mark_legends(row):
     if row['Pokemon_Name'] in legends:
         return 1
@@ -140,19 +154,28 @@ pokemon['Legendary'] = pokemon.apply(lambda x: mark_legends(x), axis=1)
 
 pokemon.to_csv('pokemon.csv')
 
+
 #-----------------------------------------------------------------------------------------------
+
+
+# Exploratory Data Analysis:
 
 pokemon = pd.read_csv('pokemon.csv', index_col = 0)
 pokemon
 
+
+# Make correlation heatmap
 plt.figure(figsize=(20,10))
 ax = sns.heatmap(pokemon.corr(), annot=True, cmap='Greens')
 bottom, top = ax.get_ylim()
 ax.set_ylim(bottom + 0.5, top - 0.5)
 
+
+# Make scatter matrix of each variable's distribution
 pd.plotting.scatter_matrix(pokemon.iloc[:, 2:], figsize=(25,10))
 
 
+# Create overlapping histograms to show different distributions of features for legendary vs non-legendary
 def multi_hist_plot(df_1, df_2, to_plot):
     fig = plt.figure(figsize=(10,5))
     plt.style.use('seaborn')
@@ -174,21 +197,25 @@ speed_hist = multi_hist_plot(df_1, df_2, 'Speed')
 height_hist = multi_hist_plot(df_1, df_2, 'Height_M')
 weight_hist = multi_hist_plot(df_1, df_2, 'Weight_KG')
 
+
 #-----------------------------------------------------------------------------------------------
 
-# Set variables in order for train test split to be done
+
+# Modeling
+
+# Set variables for train test split
 y = pokemon['Legendary']
 x = pokemon[['HP', 'Attack', 'Defense', 'Special_Attack', 'Special_Defense', 'Speed', 'Height_M', 'Weight_KG']]
 X = pd.DataFrame(x, columns = x.columns)
 
-
-# Calculate train and test variables
 X_train , X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12)
 
+# Due to class imbalance, synthesize fake data for the minority class (legendary) using SMOTE
 smote = SMOTE(sampling_strategy='minority')
 X_synth, y_synth = smote.fit_sample(X_train, y_train)
 
 
+# Create list of different classifiers/models you want to run
 classifiers = []
 
 model1 = xgboost.XGBClassifier()
@@ -203,6 +230,8 @@ classifiers.append(model3)
 model4 = RandomForestClassifier()
 classifiers.append(model4)
 
+
+# Fit each model with synthesized training data and find accuracy, precision, recall, and F1 score and print out confusion matrix and classification report
 for clf in classifiers:
     clf.fit(X_synth, y_synth)
     y_pred= clf.predict(X_test)
@@ -223,22 +252,28 @@ for clf in classifiers:
     print("")
 
 
+# Since SVM performed best, run grid search on that model to find optimal parameters
 param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000],  'kernel': ['rbf', 'linear', 'poly', 'sigmoid']}
 grid = GridSearchCV(svm.SVC(), param_grid)
 grid_search = grid.fit(X_synth, y_synth)
 
+# Optimal SVM parameters:
 print (grid_search.best_params_)
 print (grid_search.best_estimator_)
 
 grid_predictions = grid_search.predict(X_test)
 
+# Print new confusion matrix and classification report post grid search
 print(confusion_matrix(y_test, grid_predictions))
 print(classification_report(y_test, grid_predictions))
 
+# Dump model to separately reverse engineer to find names of misclassified pokémon in pokemon.py file
 model = svm.SVC(C=1000, kernel='rbf')
 model.fit(X_train, y_train)
 pickle.dump(model, open('model.pkl', 'wb'))
 
+
+# Run grid search on XGBoost model
 param_grid2 = {
     'learning_rate': [0.01, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30],
     'max_depth': [2, 4, 6, 8],
@@ -252,6 +287,7 @@ xgb_gridsearch = grid_clf.fit(X_synth, y_synth)
 
 best_parameters = grid_clf.best_params_
 
+# Optimal XGBoost parameters:
 print('Grid Search found the following optimal parameters: ')
 for param_name in sorted(best_parameters.keys()):
     print('%s: %r' % (param_name, best_parameters[param_name]))
@@ -261,26 +297,21 @@ xgb_model = xgboost.XGBClassifier(learning_rate=0.1, max_depth=6, min_child_weig
 
 test_preds = grid_clf.predict(X_test)
 
+# Print new confusion matrix and classification report post grid search on XGBoost model
 print(confusion_matrix(y_test, test_preds))
 print(classification_report(y_test, test_preds))
 
 xgb_model.fit(X_synth, y_synth)
+
+# Find feature importances for XGBoost model
 print(xgb_model.feature_importances_)
-plt.bar(range(len(xgb_model.feature_importances_)), xgb_model.feature_importances_)
-plt.show()
-
-
-plot_importance(xgb_model)
-plt.show()
-
+# Plot feature importances
 def plot_feature_importances(model):
     n_features = X_synth.shape[1]
     plt.figure(figsize=(15,10))
-    plt.barh(range(n_features), model.feature_importances_, align='center')
+    plt.barh(range(n_features), model.feature_importances_, align='center', color='green')
     plt.yticks(np.arange(n_features), X_train.columns.values)
     plt.xlabel('Feature importance')
     plt.ylabel('Feature')
 
 plot_feature_importances(xgb_model)
-
-xgboost.plot_importance(xgb_model.get_booster())
